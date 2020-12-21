14adbe404f59d17d00f4587fbd37cbca44412bf6
ben-spiller <ben-spiller@users.noreply.github.com>
2020-12-21 22:59:26 +0000

    fix build after package rename

--- src/benspiller/flashteacher/Constants.java
+++ src/benspiller/flashteacher/Constants.java
@@ -14,7 +14,7 @@ public final class Constants
 	/**
 	 * Automatically incremented by the build
 	 */
-	private static final String BUILD_NUMBER = "74";
+	private static final String BUILD_NUMBER = "76";
 	
 	/**
 	 * The full 4-part version number
